6b6a26556e57f62f485179d6c59dbaa0e0859165
EricSyntax <ericpham0902@gmail.com>
2026-08-09 15:39:38 -0700

    BankAccount

--- main.cpp
+++ main.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
-int main(){
+class bankAccount{
+    public:
+        int accountNum;
+        string firstName, lastName, pin, CVC, expirationDate;
 
-    cout << "hello";
+        void withdraw(double withdrawAmount){
+            if(withdrawAmount > balance){
+              cout <<  "Insufficient funds. Please try again";
+            }
+        };
+
+    private:
+        double balance;
+};
+
+void menu();
+
+int main(){
+    int choice;
+    bankAccount obj;
+
+    while (true){
+        menu();
+        cin >> choice;
+
+        if(choice == 1){
+            double withdrawAmount;
+            cout << "Enter a withdraw Amount\n";
+            cin >> withdrawAmount;
+            obj.withdraw(withdrawAmount);
+        }
+    }
     return 0;
+}
+
+void menu(){
+    cout << "1. Withdraw\n";
+    cout << "2.deposit\n";
+    cout << "3.check balance\n";
 }
